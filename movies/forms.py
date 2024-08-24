f37700b1af1f9a4a4029895c5891bcf8a799775a
Maria Clara G. <mariaclagouv@gmail.com>
2024-08-24 09:49:21 -0300

    feat: forms and views

--- movies/forms.py
+++ movies/forms.py
@@ -4,4 +4,7 @@ from .models import Movie
 class MovieForm(forms.ModelForm):
     class Meta:
         model = Movie
-        fields = ['']
+        fields = ['title']
+        labels = {
+            'title': 'Title'
+        }
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from .models import Movie
 import requests
+from .forms import MovieForm
 
+def fetchMovieData(request):
+    movie = None
+    form = MovieForm(request.GET or None)
+    api_key = '2f0e70a3'
 
+    if form.is_valid():
+        title = form.cleaned_data['title']
+        response = requests.get(f'http://www.omdbapi.com/?apikey={api_key}&t={title}')
+
+        if response.status_code == 200:
+            data_movie = response.json()
+            movie = Movie.objects.create(
+                title = title,
+                type = data_movie.get('type'),
+                year = data_movie.get('y'),
+                plot = data_movie.get('plot')
+            )
+    
+    return render(request, 'index.html', {'form' : form, 'movie': movie})
